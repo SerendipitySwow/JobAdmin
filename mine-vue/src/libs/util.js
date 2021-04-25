081eaed987489cc4715df19ff8d884d68e6a5e5e
X.Mo <root@imoi.cn>
2021-04-25 23:06:11 +0800

    提交代码

--- mine-vue/src/libs/util.js
+++ mine-vue/src/libs/util.js
@@ -13,8 +13,8 @@ const util = {
  * @param {String} title 标题
  */
 util.title = function (titleText) {
-  const processTitle = process.env.VUE_APP_TITLE || 'D2Admin'
-  window.document.title = `${processTitle}${titleText ? ` | ${titleText}` : ''}`
+  const processTitle = process.env.VUE_APP_TITLE || 'MineAdmin'
+  window.document.title = `${titleText ? `${titleText} - ` : ''}${processTitle}`
 }
 
 /**
--- mine-vue/src/store/modules/d2admin/modules/releases.js
+++ mine-vue/src/store/modules/d2admin/modules/releases.js
@@ -8,10 +8,9 @@ export default {
      * @param {Object} state state
      */
     versionShow () {
-      util.log.capsule('D2Admin', `v${process.env.VUE_APP_VERSION}`)
+      util.log.capsule('MineAdmin', `v${process.env.VUE_APP_VERSION}`)
       console.log('D2 Admin  https://github.com/d2-projects/d2-admin')
       console.log('Document  https://d2.pub/zh/doc/d2-admin')
-      console.log('请不要吝啬您的 star，谢谢 ~')
     }
   }
 }
