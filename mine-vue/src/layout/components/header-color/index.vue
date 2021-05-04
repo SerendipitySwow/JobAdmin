d1af0ac3d137fca51bb82960f5de4f818318d67e
X.Mo <root@imoi.cn>
2021-05-04 17:59:13 +0800

    优化代码目录

---
+++ mine-vue/src/layout/components/header-color/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-color-picker
+    class="btn-text can-hover"
+    :value="value"
+    :predefine="predefine"
+    size="mini"
+    @change="set"/>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'd2-header-color',
+  data () {
+    return {
+      predefine: [
+        '#ff4500',
+        '#ff8c00',
+        '#ffd700',
+        '#90ee90',
+        '#00ced1',
+        '#1e90ff',
+        '#c71585'
+      ]
+    }
+  },
+  computed: {
+    ...mapState('store/color', [
+      'value'
+    ])
+  },
+  watch: {
+    value (value) {
+      this.set(value)
+    }
+  },
+  methods: {
+    ...mapActions('store/color', [
+      'set'
+    ])
+  }
+}
+</script>
