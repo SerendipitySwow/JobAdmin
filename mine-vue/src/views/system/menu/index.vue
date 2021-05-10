72037c119c69ed543328e52744440e7146c8e65b
18638883567 <root@imoi.cn>
2021-05-10 17:25:26 +0800

    菜单

--- mine-vue/src/views/system/menu/index.vue
+++ mine-vue/src/views/system/menu/index.vue
@@ -1,19 +1,45 @@
 <template>
-  <ma-container>
-    asdasdasd
+  <ma-container style="padding-top:0">
+    <template slot="header">Header</template>
+    <el-table
+      v-loading="loading"
+      :data="menuTree"
+      row-key="id"
+      default-expand-all
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+      <el-table-column prop="name" label="菜单名称" :show-overflow-tooltip="true" width="160"></el-table-column>
+      <el-table-column prop="icon" label="图标" align="center" width="100">
+        <template slot-scope="scope">
+          <ma-icon :name="scope.icon"/>
+        </template>
+      </el-table-column>
+      <el-table-column prop="route" label="路由" width="180"></el-table-column>
+      <el-table-column
+        prop="code"
+        label="菜单代码">
+      </el-table-column>
+    </el-table>
   </ma-container>
 </template>
 <script>
 import { getMenuTree } from '@/api/system/menu'
 export default {
   name: 'sys-menu',
+  data () {
+    return {
+      // 遮罩层
+      loading: true,
+      menuTree: []
+    }
+  },
   mounted () {
     this.getData()
   },
   methods: {
     getData () {
       getMenuTree().then(res => {
-        console.log(res)
+        this.menuTree = res.data
+        this.loading = false
       })
     }
   }
