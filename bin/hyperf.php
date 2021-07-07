bd0ad1f27f01d665d7588f1837c294c59781fb14
X.Mo <root@imoi.cn>
2021-07-07 18:56:40 +0800

    no message

--- bin/hyperf.php
+++ bin/hyperf.php
@@ -9,6 +9,7 @@ error_reporting(E_ALL);
 
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 ! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
+! defined('START_TIME') && define('START_TIME', time());    // 启动时间
 
 require BASE_PATH . '/vendor/autoload.php';
 
