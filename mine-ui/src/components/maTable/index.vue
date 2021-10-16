4b17756604f51dccca632d80b9b058beac49d315
青衣煮茶 <root@imoi.cn>
2021-10-16 09:01:37 +0000

    maTable 新增两个回调方法

--- mine-ui/src/components/maTable/index.vue
+++ mine-ui/src/components/maTable/index.vue
@@ -87,6 +87,8 @@
 			autoLoad: { type: Boolean, default: true},
 			paginationLayout: { type: String, default: "total, prev, pager, next, jumper" },
 			showRecycle: { type: Boolean, default: false},
+			beforeQuery: { type: Function, default: () => {}},
+			afterQuery: { type: Function, default: () => {}},
 		},
 		watch: {
 			//监听从props里拿到值了
@@ -176,6 +178,10 @@
 
 				let response;
 
+				if (this.beforeQuery !== undefined) {
+					this.beforeQuery(this.$refs.scTable)
+				}
+
 				try {
 					if (this.isRecycle) {
 						// 回收站数据
@@ -187,6 +193,9 @@
 						await this.api.list(requestData).then(res => {
 							response = res
 						})
+					}
+					if (this.afterQuery !== undefined) {
+						this.afterQuery(this.$refs.scTable, response.data)
 					}
 				}catch(error){
 					this.loading = false;
