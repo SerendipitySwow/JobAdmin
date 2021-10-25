2a01e88be391413f712520411ee0e7abe54e9278
莫羿 <261091613@qq.com>
2021-10-25 17:46:51 +0800

    up

--- app/System/Database/Seeders/Update/add_cache_monitor_menu.php
+++ app/System/Database/Seeders/Update/add_cache_monitor_menu.php
@@ -17,7 +17,7 @@ class AddCacheMonitorMenu extends Seeder
     public function run()
     {
         $table = env('DB_PREFIX') . \App\System\Model\SystemMenu::getModel()->getTable();
-        $menu = "INSERT INTO `{$table}` VALUES (3700, 3000, '0,3000', '缓存监控', 'system:onlineUser', 'el-icon-paperclip', 'onlineUser', 'system/monitor/onlineUser/index', NULL, '1', 'M', '0', 98, NULL, NULL, '2021-08-08 18:26:31', '2021-08-08 18:26:31', NULL, NULL)",
+        $menu = "INSERT INTO `{$table}` VALUES (3700, 3000, '0,3000', '缓存监控', 'system:onlineUser', 'el-icon-paperclip', 'onlineUser', 'system/monitor/onlineUser/index', NULL, '1', 'M', '0', 98, NULL, NULL, '2021-08-08 18:26:31', '2021-08-08 18:26:31', NULL, NULL)";
         try {
             Db::beginTransaction();
             Db::insert($menu);
