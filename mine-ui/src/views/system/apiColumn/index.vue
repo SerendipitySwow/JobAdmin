45bf7456de32c568d1d7b75890dc79e9e5889f06
X.Mo <root@imoi.cn>
2021-11-21 21:53:01 +0800

    up

--- mine-ui/src/views/system/apiColumn/index.vue
+++ mine-ui/src/views/system/apiColumn/index.vue
@@ -1,12 +1,23 @@
 <template>
-    asdasd
+  <el-main>
+    <el-page-header icon="el-icon-arrow-left" title="返回" :content="data.title" />
+  </el-main>
 </template>
 
 <script setup>
-import { onMounted, getCurrentInstance } from 'vue'
+import { onMounted, reactive, getCurrentInstance } from 'vue'
+import useTabs from '@/utils/useTabs'
 
 const { proxy } = getCurrentInstance()
+
+let data = reactive({
+  title: ''
+})
+
 onMounted(() => {
-    console.log(proxy.$root.$route)
+    let query = proxy.$route.query
+    data.title = query.title + ' - ' + ((query.type === 'request') ? '接口请求数据' : '接口响应数据')
+    useTabs.setTitle(data.title)
+
 })
 </script>
