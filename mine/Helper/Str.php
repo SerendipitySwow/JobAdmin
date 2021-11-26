cea00b27ec11436cf1dcadc0e384fef4ba4ae673
X.Mo <root@imoi.cn>
2021-11-26 19:44:27 +0800

    up

--- mine/Helper/Str.php
+++ mine/Helper/Str.php
@@ -288,15 +288,19 @@ class Str
         return $value['years'] . '年' . $value['days'] .  '天 '. $value["hours"] . '小时'. $value['minutes'] . '分'.$value['seconds'].'秒';
 
     }
-    public static function  getUUID()
+
+    /**
+     * 生成UUID
+     * @return string
+     */
+    public static function getUUID(): string
     {
         $chars = md5(uniqid((string)mt_rand(), true));
-        $uuid = substr ( $chars, 0, 8 ) . '-'
+        return substr ( $chars, 0, 8 ) . '-'
             . substr ( $chars, 8, 4 ) . '-'
             . substr ( $chars, 12, 4 ) . '-'
             . substr ( $chars, 16, 4 ) . '-'
             . substr ( $chars, 20, 12 );
-        return $uuid ;
     }
 
     /**
