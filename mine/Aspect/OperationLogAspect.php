b660b26e8b194c525148fa489e5ac03499188051
X.Mo <root@imoi.cn>
2021-12-09 20:40:30 +0800

    操作日志注解新增直接传入菜单名称

--- mine/Aspect/OperationLogAspect.php
+++ mine/Aspect/OperationLogAspect.php
@@ -55,12 +55,16 @@ class OperationLogAspect extends AbstractAspect
      */
     public function process(ProceedingJoinPoint $proceedingJoinPoint)
     {
-        /** @var Permission $permission */
-        $permission = $proceedingJoinPoint->getAnnotationMetadata()->method[Permission::class];
+        if (isset($proceedingJoinPoint->getAnnotationMetadata()->method[OperationLog::class])) {
+            $annotation = $proceedingJoinPoint->getAnnotationMetadata()->method[OperationLog::class];
+        } else {
+            $annotation = $proceedingJoinPoint->getAnnotationMetadata()->method[Permission::class];
+        }
         $result = $proceedingJoinPoint->process();
         $evDispatcher = $this->container->get(EventDispatcherInterface::class);
         $evDispatcher->dispatch(new Operation($this->getRequestInfo([
-            'code' => $permission->menuCode,
+            'code' => $annotation->menuCode ?? '',
+            'name' => $annotation->menuName ?? '',
             'response_code' => $result->getStatusCode(),
             'response_data' => $result->getBody()->getContents()
         ])));
@@ -85,7 +89,7 @@ class OperationLogAspect extends AbstractAspect
             'protocol' => $request->getServerParams()['server_protocol'],
             'ip' => $request->ip(),
             'ip_location' => Str::ipToRegion($request->ip()),
-            'service_name' => $this->getOperationMenuName($data['code']),
+            'service_name' => $data['name'] ?: $this->getOperationMenuName($data['code']),
             'request_data' => $request->all(),
             'response_code' => $data['response_code'],
             'response_data' => $data['response_data'],
